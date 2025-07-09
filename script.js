2d1e6701fd31bc7c3f05d96e350207c1efbc074d
Rediet-Ferew <redietasnakech@gmail.com>
2025-07-09 17:14:59 +0300

    script addition

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Welcome to SnapSite!");
+// Load widget globally on all pages
+(function (w, d, i) {
+  w._settings = {
+    site: i,
+    baseUrl: 'https://vueclick.com'
+  };
+  var h = d.getElementsByTagName('head')[0];
+  var s = d.createElement('script');
+  s.async = 1;
+  s.src = 'https://vueclick.com/widget.js';
+  s.crossOrigin = 'anonymous';
+  h.appendChild(s);
+})(window, document, 'a0d54c09-1cc3-4b24-b554-6f81206fa384');
